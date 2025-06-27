40b8c0448e9755d3b731d35fd8395e1a19b7ac1c
Lívia Lelis <git@lelis.dev>
2025-06-27 15:00:27 -0300

    handle /femiguel and react messages with 🗿

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use dotenv::dotenv;
-use teloxide::prelude::*;
+use teloxide::{prelude::*, types::ReactionType};
+use regex::Regex;
 
 const MIGUEL: &str = "
 PAROU, PAROU A DISCUSSÃO
 
 miguwu ><
 ";
+
 
 #[tokio::main]
 async fn main() {
@@ -21,11 +23,30 @@ async fn main() {
 }
 
 async fn answer(bot: Bot, msg: Message) -> ResponseResult<()> {
-    log::info!("Received message: {:?}", msg);
+    let miguel_re: Regex = match Regex::new(r"/[A-z0-9À-ÿ]*?miguel[A-z0-9À-ÿ]*") {
+        Ok(re) => re,
+        Err(e) => {
+            log::error!("Error creating regex: {:?}", e);
+            return Ok(());
+        }
+    };
+
     let text = msg.text().unwrap_or_default().to_lowercase();
-    if !text.contains("/miguel") {
-        return Ok(());
+
+    if miguel_re.is_match(&text) {
+        handle_miguel_command(&bot, &msg).await?;
     }
+
+    if text.contains("miguel") {
+        handle_miguel_message(&bot, &msg).await?;
+    }
+
+    Ok(())
+}
+
+async fn handle_miguel_command(bot: &Bot, msg: &Message) -> ResponseResult<()> {
+    log::info!("Received **miguel** command: {:?}", msg);
+
     match msg.thread_id {
         Some(thread_id) => {
             bot.send_message(msg.chat.id, MIGUEL)
@@ -36,6 +57,17 @@ async fn answer(bot: Bot, msg: Message) -> ResponseResult<()> {
             bot.send_message(msg.chat.id, MIGUEL).await?;
         }
     }
+
+    Ok(())
+}
+
+async fn handle_miguel_message(bot: &Bot, msg: &Message) -> ResponseResult<()> {
+    log::info!("A wild **miguel** appeared: {:?}", msg);
+
+
+    bot.set_message_reaction(msg.chat.id, msg.id)
+        .reaction([ReactionType::Emoji { emoji: "🗿".to_string() }])
+        .await?;
 
     Ok(())
 }
