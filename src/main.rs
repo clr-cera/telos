464698867eccfc3af4ec2b810100cbd8d134904b
clr <clrcera05@gmail.com>
2025-06-26 22:02:08 -0300

    telos

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use dotenv::dotenv;
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+const MIGUEL: &str = "
+__  __ _____ _____ _    _ ______ _
+|  \\/  |_   _/ ____| |  | |  ____| |
+| \\  / | | || |  __| |  | | |__  | |
+| |\\/| | | || | |_ | |  | |  __| | |
+| |  | |_| || |__| | |__| | |____| |____
+|_|  |_|_____\\_____|\\____/|______|______|
+";
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+    log::info!("Starting bot...");
+
+    let bot = Bot::from_env();
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
+enum Command {
+    #[command(description = "Say hello")]
+    Hello,
+    #[command(description = "Miguel")]
+    Miguel,
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Hello => {
+            bot.send_message(msg.chat.id, "Hello!").await?;
+        }
+        Command::Miguel => {
+            bot.send_message(msg.chat.id, MIGUEL).await?;
+        }
+    }
+    Ok(())
+}
