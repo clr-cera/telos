b8d00b8633630f0f8859d9ad7862f04a72179640
Lívia Lelis <git@lelis.dev>
2025-06-30 12:49:05 -0300

    feature: whitelist/admin implementation with sqlx persistence

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM rust:latest as builder
+FROM rust:latest AS builder
 
 WORKDIR /app
 RUN apt-get update -y && apt-get install libssl-dev -y
 
 COPY Cargo.toml Cargo.lock ./
 
-# INstall the actual application
+# Install the actual application
 COPY src ./src
+COPY migrations ./migrations
 RUN cargo build --release
 
-FROM debian:latest as runner
+FROM debian:latest AS runner
 WORKDIR /app
 RUN apt-get update -y && apt-get install openssl ca-certificates -y
 COPY --from=builder /app/target/release/telos .
-COPY .env .
 CMD ["/app/telos"]
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,85 @@
+use std::{env, process::exit, sync::Arc};
+
 use dotenv::dotenv;
-use teloxide::{prelude::*, types::ReactionType};
-use regex::Regex;
+use teloxide::prelude::*;
 
-const MIGUEL: &str = "
-PAROU, PAROU A DISCUSSÃO
+mod db;
+mod handler;
+mod admin;
+mod miguel;
 
-miguwu ><
-";
+async fn create_db() -> Result<db::DB, Box<dyn std::error::Error>> {
+    let path = env::var("DATABASE_PATH").unwrap_or_else(|_| "sqlite://db.sqlite?mode=rwc".to_string());
 
-const MIGUEL_USER: &str = "migeyel";
+    let db = db::DB::new(&path).await?;
+    db.migrate().await?;
 
+    log::info!("Database created at {:?}", path);
+
+    Ok(db)
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
     pretty_env_logger::init();
+
     log::info!("Starting bot...");
 
-    let bot = Bot::from_env();
-    let schema = Update::filter_message().branch(Message::filter_text().endpoint(answer));
-
-    Dispatcher::builder(bot, schema).build().dispatch().await;
-}
-
-async fn answer(bot: Bot, msg: Message) -> ResponseResult<()> {
-    let text = msg.text().unwrap_or_default().to_lowercase();
-
-
-    handle_miguel_command(&bot, &msg, &text).await?;
-    handle_miguel_message(&bot, &msg, &text).await?;
-    
-
-    Ok(())
-}
-
-async fn handle_miguel_command(bot: &Bot, msg: &Message, text: &str) -> ResponseResult<()> {
-    let miguel_re: Regex = match Regex::new(r"/[A-z0-9À-ÿ]*?miguel[A-z0-9À-ÿ]*") {
-        Ok(re) => re,
+    let token = match env::var("TELOXIDE_TOKEN") {
+        Ok(token) => token,
         Err(e) => {
-            log::error!("Error creating regex: {:?}", e);
-            return Ok(());
+            log::error!("Error getting TELOXIDE_TOKEN: {:?}", e);
+            exit(1);
         }
     };
 
-    if !miguel_re.is_match(text) {
-        return Ok(());
-    }
+    let bot = Bot::new(token);
 
-    log::info!("Received **miguel** command: {:?}", msg);
-
-    match msg.thread_id {
-        Some(thread_id) => {
-            bot.send_message(msg.chat.id, MIGUEL)
-                .message_thread_id(thread_id)
-                .await?;
+    let db = match create_db().await {
+        Ok(db) => db,
+        Err(e) => {
+            log::error!("Error creating database: {:?}", e);
+            exit(1);
         }
+    };
+
+
+    let miguel_handler = match miguel::MiguelHandler::new() {
+        Some(handler) => handler,
         None => {
-            bot.send_message(msg.chat.id, MIGUEL).await?;
+            log::error!("Error creating miguel handler");
+            exit(1);
         }
-    }
+    };
 
-    Ok(())
-}
+    let admin_handler = match admin::AdminHandler::new(db.clone()) {
+        Some(handler) => handler,
+        None => {
+            log::error!("Error creating admin handler");
+            exit(1);
+        }
+    };
 
-async fn handle_miguel_message(bot: &Bot, msg: &Message, text: &str) -> ResponseResult<()> {
-    // Extract message sender
-    if msg.from.is_none() {
-        return Ok(());
-    }
+    let the_handler = Arc::new(handler::Handler::new(miguel_handler, admin_handler));
 
-    let from = msg.from.as_ref().unwrap().clone();
+    let schema = {
+        let handler_clone = Arc::clone(&the_handler); // move clone into closure
 
-    // Check if message is from miguel
-    if 
-        from.username.is_none()
-        || from.username.unwrap() != MIGUEL_USER
-    {
-        return Ok(());
-    }
+        Update::filter_message().branch(
+            Message::filter_text().endpoint(
+                move |bot: Bot, msg: Message| {
+                    let handler_clone = Arc::clone(&handler_clone); // clone inside closure
 
+                    async move {
+                        handler_clone.handle(&bot, &msg).await?;
+                        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+                    }
+                },
+            ),
+        )
+    };
 
-
-    // Check if this is a "miguel" message
-    if !text.contains("miguel") && !text.contains("miguwu") {
-        return Ok(());
-    }
-
-
-    // Received a miguel, react with 🗿
-    log::info!("A wild **miguel** appeared: {:?}", msg);
-
-    bot.set_message_reaction(msg.chat.id, msg.id)
-        .reaction([ReactionType::Emoji { emoji: "🗿".to_string() }])
-        .await?;
-
-    Ok(())
+    Dispatcher::builder(bot, schema).build().dispatch().await;
 }
